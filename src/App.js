6dc3a90da0b824bcff4e7f2794ad51b0f1339bc6
Ivo Georgiev <ivo@strem.io>
2019-12-23 14:27:57 +0000

    add adex-brand and logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import { themeMUI } from './themeMUi'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import logo from 'adex-brand/logos/adex-white.svg'
 const NavBar = () => {
     return(
-        <div>
+        <MuiThemeProvider theme={themeMUI}>
         <AppBar position="static">
             <Toolbar>
+		<img width="100px" src={logo} alt="logo"></img>
                 <Typography variant="title" color="inherit">
-			AdEx Staking UI
+			Staking
 	    	</Typography>
             </Toolbar>
         </AppBar>
-        </div>
+        </MuiThemeProvider>
     )
 }
 
