4076ddee916f9d46117daac43d000ed900a3c329
Ivo Georgiev <ivo@strem.io>
2019-12-23 22:51:19 +0000

    split loadBonds

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import { themeMUI } from './themeMUi'
 import AppBar from '@material-ui/core/AppBar'
@@ -7,6 +7,7 @@ import Typography from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
 import logo from 'adex-brand/logos/adex-white.svg'
 import { Contract, getDefaultProvider } from 'ethers'
 import { Web3Provider } from 'ethers/providers'
@@ -35,14 +36,16 @@ function NavBar() {
 }
 
 export default function App() {
+	const [count, setCount] = useState(0)
 	useEffect(() => {
 		loadStats()
 			.then(console.log)
 
-	})
+	}, [])
 	return (
 		<MuiThemeProvider theme={themeMUI}>
 			<NavBar />
+			<Button onClick={() => setCount(count+1)}>{count}</Button>
 			<Grid container spacing={2}>
 			{[1,2,3,4].map(x => (
 				<Grid key={x} item xs={3}>
@@ -58,17 +61,26 @@ export default function App() {
 }
 
 async function loadStats() {
-	let addr = null
-	if (window.web3) {
-		const provider = new Web3Provider(window.web3.currentProvider)
-		const signer = provider.getSigner()
-		addr = await signer.getAddress()
-	}
-	const [ totalStake, logsBond, logsUnbondReq, logsUnbonded ] = await Promise.all([
+	const [ totalStake, userBonds ] = await Promise.all([
 		Token.balanceOf(ADDR_STAKING),
+		loadUserBonds()
+	])
+	return { totalStake, userBonds }
+}
+
+async function loadUserBonds() {
+	if (!window.web3) return []
+
+	const provider = new Web3Provider(window.web3.currentProvider)
+	const signer = provider.getSigner()
+	const addr = await signer.getAddress()
+	// @TODO calculate bond ID from the stuff in LogBond
+	//const bondId = () => 
+
+	// @TODO: we can get all of them in one call to getLogs
+	const [ logsBond, logsUnbondReq, logsUnbonded ] = await Promise.all([
 		provider.getLogs({ fromBlock: 0, ...Staking.filters.LogBond(addr) }),
 		provider.getLogs({ fromBlock: 0, ...Staking.filters.LogUnbondRequested(addr) }),
 		provider.getLogs({ fromBlock: 0, ...Staking.filters.LogUnbonded(addr) }),
 	])
-	return { totalStake, logsBond }
 }
