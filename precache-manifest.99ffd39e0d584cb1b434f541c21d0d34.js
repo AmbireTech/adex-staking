c26f9fdc9628a4b3dca7ec09e2a03028b369db82
Ivo Georgiev <ivo@strem.io>
2019-12-25 21:06:51 +0000

    Updates

---
+++ precache-manifest.99ffd39e0d584cb1b434f541c21d0d34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c75701b38b8c12de126bf5a42ff3c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84e2d7df58ec6e546cdc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d21ffbb838741a226ab9",
+    "url": "/static/js/2.5042569c.chunk.js"
+  },
+  {
+    "revision": "80ab6de57c59f2d36351e7a51a9b6724",
+    "url": "/static/js/2.5042569c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84e2d7df58ec6e546cdc",
+    "url": "/static/js/main.5b3e6adf.chunk.js"
+  },
+  {
+    "revision": "c429033346747568ccdc",
+    "url": "/static/js/runtime-main.02425944.js"
+  },
+  {
+    "revision": "bf64bad2f09d9fa62fce87147171a9db",
+    "url": "/static/media/adex-staking.bf64bad2.svg"
+  }
+]);
