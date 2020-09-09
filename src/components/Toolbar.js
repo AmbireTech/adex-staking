149c42dd24b677c0b2d8bcfcb217cb70349d4e99
IvoPaunov <ivo.paunov@gmail.com>
2020-09-09 17:50:41 +0300

    fix toolbar styles

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -16,6 +16,28 @@ import {
 import HelperMenu from "./HelperMenu"
 import logo from "./../adex-staking.svg"
 import { themeMUI } from "./../themeMUi"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles(theme => {
+	return {
+		appBar: {
+			top: 0,
+			left: 0,
+			right: 0,
+			width: "auto",
+			position: "fixed",
+			[theme.breakpoints.up("md")]: {
+				left: 250
+			}
+		},
+		toolbar: {
+			flexFlow: "wrap",
+			height: 69,
+			paddingRight: 0,
+			paddingLeft: 0
+		}
+	}
+})
 
 export const AppToolbar = ({
 	chosenWalletType,
@@ -23,48 +45,52 @@ export const AppToolbar = ({
 	setNewBondOpen,
 	stats,
 	handleDrawerToggle
-}) => (
-	<AppBar position="static">
-		<Toolbar>
-			<Hidden mdUp>
-				<Box pl={1}>
-					<IconButton
-						color="inherit"
-						aria-label="open drawer"
-						onClick={handleDrawerToggle}
+}) => {
+	const classes = useStyles()
+
+	return (
+		<AppBar className={classes.appBar} position="static">
+			<Toolbar>
+				<Hidden mdUp>
+					<Box pl={1}>
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							onClick={handleDrawerToggle}
+						>
+							<MenuIcon />
+						</IconButton>
+					</Box>
+				</Hidden>
+				<img height="40vh" src={logo} alt="logo"></img>
+				{chosenWalletType && (
+					<Fab
+						disabled={!stats.loaded}
+						onClick={() => setNewBondOpen(true)}
+						variant="extended"
+						color="secondary"
+						style={{ position: "absolute", right: "5%", top: "50%" }}
 					>
-						<MenuIcon />
-					</IconButton>
-				</Box>
-			</Hidden>
-			<img height="40vh" src={logo} alt="logo"></img>
-			{chosenWalletType && (
-				<Fab
-					disabled={!stats.loaded}
-					onClick={() => setNewBondOpen(true)}
-					variant="extended"
-					color="secondary"
-					style={{ position: "absolute", right: "5%", top: "50%" }}
-				>
-					<AddIcon style={{ margin: themeMUI.spacing(1) }} />
-					{"Stake your ADX"}
-				</Fab>
-			)}
-			{!chosenWalletType && (
-				<Fab
-					onClick={() => setConnectWallet(true)}
-					variant="extended"
-					color="secondary"
-					style={{ position: "absolute", right: "5%", top: "50%" }}
-				>
-					<AccountBalanceWalletIcon style={{ margin: themeMUI.spacing(1) }} />
-					{"Connect Wallet"}
-				</Fab>
-			)}
-			{HelperMenu()}
-		</Toolbar>
-	</AppBar>
-)
+						<AddIcon style={{ margin: themeMUI.spacing(1) }} />
+						{"Stake your ADX"}
+					</Fab>
+				)}
+				{!chosenWalletType && (
+					<Fab
+						onClick={() => setConnectWallet(true)}
+						variant="extended"
+						color="secondary"
+						style={{ position: "absolute", right: "5%", top: "50%" }}
+					>
+						<AccountBalanceWalletIcon style={{ margin: themeMUI.spacing(1) }} />
+						{"Connect Wallet"}
+					</Fab>
+				)}
+				{HelperMenu()}
+			</Toolbar>
+		</AppBar>
+	)
+}
 
 AppToolbar.propTypes = {
 	chosenWalletType: PropTypes.string,
