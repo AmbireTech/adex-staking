0e8f88f229ce69120711cfae0d4840e8a1e49495
IvoPaunov <ivo.paunov@gmail.com>
2020-09-09 18:36:16 +0300

    update stats card styles

--- src/components/StatsCard.js
+++ src/components/StatsCard.js
@@ -1,31 +1,22 @@
 import React from "react"
-import { Paper } from "@material-ui/core"
+import { Box, Divider } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
 import LinearProgress from "@material-ui/core/LinearProgress"
 import Typography from "@material-ui/core/Typography"
-import { themeMUI } from "../themeMUi"
 
 export default function StatsCard({ title, subtitle, extra, loaded, actions }) {
-	const extraElem =
-		typeof extra === "string" ? (
-			<Typography color="primary" variant="h6">
-				{extra}
-			</Typography>
-		) : (
-			extra || <></>
-		)
 	return (
-		<Paper elevation={3} style={{ height: "100%" }}>
-			<div style={{ padding: themeMUI.spacing(2), minHeight: "85px" }}>
-				<Typography variant={subtitle.length > 16 ? "h6" : "h5"}>
-					{subtitle}
-				</Typography>
-				{extraElem}
-				<Typography color="textSecondary" variant="subtitle2">
-					{title}
-				</Typography>
-				{actions || <></>}
-			</div>
+		<Box elevation={3} style={{ height: "100%" }}>
+			<Typography variant="caption" display="block">
+				{title}
+			</Typography>
+
+			<Typography color="textSecondary" variant="subtitle2">
+				{subtitle} {extra}
+			</Typography>
+			{actions || <></>}
+
 			{!loaded ? <LinearProgress /> : <></>}
-		</Paper>
+		</Box>
 	)
 }
