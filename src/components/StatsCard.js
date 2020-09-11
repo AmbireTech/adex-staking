b49554ca0f41f9416beae041e74a88015bd2f96f
IvoPaunov <ivo.paunov@gmail.com>
2020-09-11 16:49:41 +0300

    update stats card

--- src/components/StatsCard.js
+++ src/components/StatsCard.js
@@ -1,19 +1,99 @@
 import React from "react"
-import { Box, Divider } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
+import { Box, Tooltip } from "@material-ui/core"
 import LinearProgress from "@material-ui/core/LinearProgress"
 import Typography from "@material-ui/core/Typography"
+import { InfoSharp as InfoIcon } from "@material-ui/icons"
 
-export default function StatsCard({ title, subtitle, extra, loaded, actions }) {
+function Info({ title, color, icon }) {
 	return (
-		<Box elevation={3} style={{ height: "100%" }}>
-			<Typography variant="caption" display="block">
-				{title}
-			</Typography>
+		<Tooltip title={title}>
+			<InfoIcon fontSize="inherit" />
+		</Tooltip>
+	)
+}
 
-			<Typography color="textSecondary" variant="subtitle2">
-				{subtitle} {extra}
-			</Typography>
+function CardRow({ text, infoText, fontWeight, fontSize, color }) {
+	return (
+		<Typography component="div" variant="body2">
+			<Box
+				color={color}
+				fontWeight={fontWeight}
+				fontSize={fontSize}
+				display="flex"
+				flexDirection="row"
+				alignItems="center"
+			>
+				<Box>{text}</Box>
+				{infoText && (
+					<Box
+						ml={0.69}
+						display="flex"
+						// flexDirection='row'
+						// alignItems='center'
+					>
+						<Info title={infoText} />
+					</Box>
+				)}
+			</Box>
+		</Typography>
+	)
+}
+
+export default function StatsCard({
+	size,
+	title,
+	titleInfo,
+	subtitle,
+	subtitleInfo,
+	extra,
+	extraInfo,
+	moreExtra,
+	moreExtraInfo,
+	loaded,
+	actions
+}) {
+	return (
+		<Box>
+			{title && (
+				<CardRow
+					color="text.primary"
+					fontWeight={size === "large" ? "fontWeightBold" : "fontWeightRegular"}
+					fontSize={size === "large" ? 16 : 13}
+					text={title}
+					infoText={titleInfo}
+				/>
+			)}
+
+			{subtitle && (
+				<CardRow
+					color="warning.main"
+					fontWeight={size === "large" ? "fontWeightBold" : "fontWeightRegular"}
+					fontSize={size === "large" ? 19 : 15}
+					text={subtitle}
+					infoText={subtitleInfo}
+				/>
+			)}
+
+			{extra && (
+				<CardRow
+					color="text.primary"
+					fontWeight={size === "large" ? "fontWeightBold" : "fontWeightRegular"}
+					fontSize={size === "large" ? 16 : 13}
+					text={extra}
+					infoText={extraInfo}
+				/>
+			)}
+
+			{moreExtra && (
+				<CardRow
+					color="text.primary"
+					fontWeight={size === "large" ? "fontWeightBold" : "fontWeightRegular"}
+					fontSize={size === "large" ? 16 : 13}
+					text={moreExtra}
+					infoText={moreExtraInfo}
+				/>
+			)}
+
 			{actions || <></>}
 
 			{!loaded ? <LinearProgress /> : <></>}
