a9e98804e2dfa2b1cbb20fa4e2d56250b4daf15d
IvoPaunov <ivo.paunov@gmail.com>
2020-09-14 18:01:23 +0300

    update wallet menu

--- src/components/WalletMenu.js
+++ src/components/WalletMenu.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useContext } from "react"
 import AppContext from "../AppContext"
 import { makeStyles } from "@material-ui/core/styles"
-import { Chip, Fab } from "@material-ui/core"
+import { Chip, Fab, Avatar } from "@material-ui/core"
 import { AccountBalanceWalletSharp as AccountBalanceWalletIcon } from "@material-ui/icons"
 import Jazzicon, { jsNumberForAddress } from "react-jazzicon"
 import { formatAddress } from "../helpers/formatting"
@@ -19,26 +19,32 @@ export const Wallet = () => {
 
 	return (
 		<Fragment>
-			{!chosenWalletType.name && (
+			{!chosenWalletType.name || !stats.connectedWalletAddress ? (
 				<Fab
 					onClick={() => setConnectWallet(true)}
 					variant="extended"
 					color="secondary"
+					size="small"
+					disabled={chosenWalletType.name && !stats.connectedWalletAddress}
 				>
 					<AccountBalanceWalletIcon className={classes.fabIcon} />
 					{"Connect Wallet"}
 				</Fab>
+			) : (
+				<Chip
+					avatar={
+						stats.connectedWalletAddress ? (
+							<Avatar>
+								<Jazzicon
+									diameter={26}
+									seed={jsNumberForAddress(stats.connectedWalletAddress)}
+								/>
+							</Avatar>
+						) : null
+					}
+					label={formatAddress(stats.connectedWalletAddress)}
+				/>
 			)}
-
-			<Chip
-				avatar={
-					<Jazzicon
-						diameter={26}
-						seed={jsNumberForAddress(stats.connectedWalletAddress || "")}
-					/>
-				}
-				label={formatAddress(stats.connectedWalletAddress)}
-			/>
 		</Fragment>
 	)
 }
