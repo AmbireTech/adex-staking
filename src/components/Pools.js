6a761a34a2802d10ca86395b18f13dea3a891836
IvoPaunov <ivo.paunov@gmail.com>
2020-09-15 17:25:30 +0300

    update pools styles and data

--- src/components/Pools.js
+++ src/components/Pools.js
@@ -1,10 +1,11 @@
 import React, { useContext } from "react"
 import AppContext from "../AppContext"
 import { makeStyles } from "@material-ui/core/styles"
-import { Fab, Box, Typography } from "@material-ui/core"
+import { Fab, Box, Typography, SvgIcon } from "@material-ui/core"
 import { AddSharp as AddIcon } from "@material-ui/icons"
 import PoolCard from "./PoolCard"
 import { getApproxAPY, formatADXPretty } from "../helpers/formatting"
+import { ReactComponent as TomIcon } from "./../resources/tom-ic.svg"
 
 const useStyles = makeStyles(theme => ({
 	fabIcon: {
@@ -16,21 +17,21 @@ const Pools = () => {
 	const classes = useStyles()
 
 	const { stats, setNewBondOpen, chosenWalletType } = useContext(AppContext)
+	const canStake = !!chosenWalletType.name && !!stats.connectedWalletAddress
 
 	return (
 		<Box>
-			<Box>
+			<Box display="flex" flexDirection="row" justifyContent="space-between">
 				<Box>
 					<Typography variant="h2">{"Pools"}</Typography>
 				</Box>
 				<Box>
-					{!chosenWalletType.name && (
+					{chosenWalletType.name && (
 						<Fab
 							disabled={!stats.loaded}
 							onClick={() => setNewBondOpen(true)}
 							variant="extended"
 							color="secondary"
-							style={{ position: "absolute", right: "5%", top: "50%" }}
 						>
 							<AddIcon className={classes.fabIcon} />
 							{"Stake your ADX"}
@@ -38,14 +39,21 @@ const Pools = () => {
 					)}
 				</Box>
 			</Box>
-			<Box>
+			<Box mt={4}>
 				<PoolCard
-					icon={"tom"}
+					icon={
+						<SvgIcon fontSize="large" color="inherit">
+							<TomIcon width="100%" height="100%" color="secondary" />
+						</SvgIcon>
+					}
 					name={"Tom"}
-					totalStakedADX={formatADXPretty(stats.totalStake)}
-					currentAPY={`${(getApproxAPY(null, stats.totalStake) * 100).toFixed(
-						2
-					)}% APY`}
+					totalStakedADX={formatADXPretty(stats.totalStakeTom)}
+					currentAPY={`${(
+						getApproxAPY(null, stats.totalStakeTom) * 100
+					).toFixed(2)}% APY`}
+					onStakeBtnClick={() => setNewBondOpen(true)}
+					loading={!stats.loaded}
+					disabled={!canStake}
 				/>
 			</Box>
 		</Box>
