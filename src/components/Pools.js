95f3679b403e91766874410c363feb162689e7ef
IvoPaunov <ivo.paunov@gmail.com>
2020-09-15 18:20:11 +0300

    update pools component to use section header

--- src/components/Pools.js
+++ src/components/Pools.js
@@ -1,44 +1,18 @@
 import React, { useContext } from "react"
 import AppContext from "../AppContext"
-import { makeStyles } from "@material-ui/core/styles"
-import { Fab, Box, Typography, SvgIcon } from "@material-ui/core"
-import { AddSharp as AddIcon } from "@material-ui/icons"
+import { Box, SvgIcon } from "@material-ui/core"
 import PoolCard from "./PoolCard"
 import { getApproxAPY, formatADXPretty } from "../helpers/formatting"
 import { ReactComponent as TomIcon } from "./../resources/tom-ic.svg"
-
-const useStyles = makeStyles(theme => ({
-	fabIcon: {
-		marginRight: theme.spacing(1)
-	}
-}))
+import SectionHeader from "./SectionHeader"
 
 const Pools = () => {
-	const classes = useStyles()
-
 	const { stats, setNewBondOpen, chosenWalletType } = useContext(AppContext)
 	const canStake = !!chosenWalletType.name && !!stats.connectedWalletAddress
 
 	return (
 		<Box>
-			<Box display="flex" flexDirection="row" justifyContent="space-between">
-				<Box>
-					<Typography variant="h2">{"Pools"}</Typography>
-				</Box>
-				<Box>
-					{chosenWalletType.name && (
-						<Fab
-							disabled={!stats.loaded}
-							onClick={() => setNewBondOpen(true)}
-							variant="extended"
-							color="secondary"
-						>
-							<AddIcon className={classes.fabIcon} />
-							{"Stake your ADX"}
-						</Fab>
-					)}
-				</Box>
-			</Box>
+			<SectionHeader title={"Pools"} />
 			<Box mt={4}>
 				<PoolCard
 					icon={
