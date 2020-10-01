20371671a09fb6757cf7cab6895e6285c59f91f7
IvoPaunov <ivo.paunov@gmail.com>
2020-10-01 19:55:53 +0300

    fix loadDepositsStats

--- src/actions/loyaltyPoolActions.js
+++ src/actions/loyaltyPoolActions.js
@@ -1,25 +1,29 @@
 import { Contract } from "ethers"
 import ERC20ABI from "../abi/ERC20"
+import ADXLoyaltyPoolTokenABI from "../abi/ADXLoyaltyPoolToken"
 import { ADDR_ADX, ADDR_ADX_LOYALTY_TOKEN, ZERO } from "../helpers/constants"
 import { getSigner, defaultProvider } from "../ethereum"
 
 const provider = defaultProvider
 const Token = new Contract(ADDR_ADX, ERC20ABI, provider)
-const LoyaltyToken = new Contract(ADDR_ADX_LOYALTY_TOKEN, ERC20ABI, provider)
+const LoyaltyToken = new Contract(
+	ADDR_ADX_LOYALTY_TOKEN,
+	ADXLoyaltyPoolTokenABI,
+	provider
+)
 const ZERO_ADDR = "0x0000000000000000000000000000000000000000"
+const ADX_LP_TOKEN_DECIMALS_MUL = "1000000000000000000"
 
 export async function loadDepositsStats(walletAddr) {
 	walletAddr = "0xd6e371526cdaee04cd8af225d42e37bc14688d9e"
 	const [
-		totalADX,
 		balanceLpToken,
-		totalSupplyLPT,
+		currentShareValue,
 		loyaltyTokenTransfersLogs,
 		adexTokenTransfersLogs
 	] = await Promise.all([
-		Token.balanceOf(ADDR_ADX_LOYALTY_TOKEN),
-		LoyaltyToken.totalSupply(),
 		LoyaltyToken.balanceOf(walletAddr),
+		LoyaltyToken.shareValue(),
 		provider.getLogs({
 			fromBlock: 0,
 			...LoyaltyToken.filters.Transfer(null, walletAddr, null)
@@ -30,7 +34,9 @@ export async function loadDepositsStats(walletAddr) {
 		})
 	])
 
-	const balanceLpADX = balanceLpToken.mul(totalADX).div(totalSupplyLPT)
+	const balanceLpADX = balanceLpToken
+		.mul(currentShareValue)
+		.div(ADX_LP_TOKEN_DECIMALS_MUL)
 
 	const currentBalance = {
 		balanceLpToken,
@@ -41,9 +47,9 @@ export async function loadDepositsStats(walletAddr) {
 		log => LoyaltyToken.interface.parseLog(log).values[0] !== ZERO_ADDR
 	)
 
-	// rewards === null => unknown rewards - can not be calculated
+	// reward === null => unknown reward - can not be calculated
 	if (hasExternalLoyaltyTokenTransfers) {
-		currentBalance.rewards = null
+		currentBalance.reward = null
 		return currentBalance
 	}
 
@@ -69,11 +75,17 @@ export async function loadDepositsStats(walletAddr) {
 		{ adx: ZERO, adxLPT: ZERO }
 	)
 
-	const avgDepositPrice = userDeposits.adxLPT.div(userDeposits.adx)
+	// multiply by decimals to keep the precision
+	const avgDepositShareValue = userDeposits.adx
+		.mul(ADX_LP_TOKEN_DECIMALS_MUL)
+		.div(userDeposits.adxLPT)
+	const reward = balanceLpToken
+		.mul(currentShareValue.sub(avgDepositShareValue))
+		.div(ADX_LP_TOKEN_DECIMALS_MUL)
 
-	const depositedADX = balanceLpADX.div(avgDepositPrice)
-
-	const reward = balanceLpADX - depositedADX
+	// console.log('reward', reward.toString())
+	// console.log('balanceLpToken', balanceLpToken.toString())
+	// console.log('balanceLpADX', balanceLpADX.toString())
 
 	currentBalance.reward = reward
 
