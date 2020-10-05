2e90941185f2c324654d44ded13cc17dc33f0a54
Rangel Stoilov <rangelstoilov@gmail.com>
2020-10-05 18:03:46 +0300

    cleanup unused components

--- src/components/EmailSignUpCard.js
+++ src/components/EmailSignUpCard.js
@@ -3,27 +3,21 @@ import { makeStyles } from "@material-ui/core/styles"
 import {
 	Box,
 	Button,
-	CircularProgress,
 	Typography,
-	SvgIcon,
-	Input,
-	FormGroup,
 	Checkbox,
 	FormControlLabel,
 	TextField,
 } from "@material-ui/core"
-import { ReactComponent as ComingSoonImg } from "./../resources/coming-soon-ic.svg"
 import { CardRow } from "./cardCommon"
-import Tooltip from "./Tooltip"
 import { ReactComponent as EmailAwardsIcon } from "./../resources/mail-awards.svg"
 
 const useStyles = makeStyles((theme) => {
 	return {
-		iconBox: (props) => ({
+		iconBox: {
 			borderRadius: "100%",
 			position: "absolute",
-			width: props.iconSize || 160,
-			height: props.iconSize || 160,
+			width: 160,
+			height: 160,
 			top: -theme.spacing(4),
 			backgroundColor: "transparent",
 			color: theme.palette.background.default,
@@ -31,25 +25,6 @@ const useStyles = makeStyles((theme) => {
 			flexDirection: "column",
 			alignItems: "center",
 			justifyContent: "center",
-		}),
-		overlay: {
-			position: "absolute",
-			left: 0,
-			top: 0,
-			right: 0,
-			bottom: 0,
-			backgroundColor: theme.palette.overlay,
-		},
-		loading: {
-			position: "absolute",
-			width: "100%",
-			height: "100%",
-			top: 0,
-			left: 0,
-		},
-		comingSoon: {
-			width: 160,
-			height: "auto",
 		},
 		bold: {
 			fontWeight: 800,
@@ -58,32 +33,14 @@ const useStyles = makeStyles((theme) => {
 			backgroundColor: theme.palette.text.main,
 			borderRadius: 20,
 		},
+		gdprCheckbox: {
+			fontSize: 11,
+		},
 	}
 })
 
-export default function EmailSignUp({
-	poolId,
-	icon,
-	name,
-	totalStakedADX,
-	totalStakedUSD,
-	currentAPY,
-	weeklyYield,
-	weeklyYieldInfo,
-	onStakeBtnClick,
-	loading,
-	disabled,
-	disabledInfo,
-	lockupPeriodTitle,
-	lockupPeriodInfo,
-	lockupPeriod,
-	loaded,
-	actions,
-	iconBg,
-	iconSize,
-	comingSoon,
-}) {
-	const classes = useStyles({ iconBg, iconSize })
+export default function EmailSignUp({}) {
+	const classes = useStyles()
 
 	return (
 		<Box
@@ -127,166 +84,23 @@ export default function EmailSignUp({
 					fullWidth
 				/>
 			</Box>
-			<Box>
+			<Box mt={1}>
 				<FormControlLabel
-					control={
-						<Checkbox
-							// checked={state.checkedA}
-							// onChange={handleChange}
-							name="checkedA"
-						/>
-					}
-					label="Secondary"
+					classes={{ label: classes.gdprCheckbox }}
+					control={<Checkbox size="small" name="checkedA" />}
+					label={`Yes, I want AdEx Network to send me news and other related content`}
 				/>
 			</Box>
 			<Box width={1} mt={3} display="flex" justifyContent="center">
-				<Button id={`sign-up`} className={classes.singUp} variant="contained">
+				<Button
+					id={`sign-up`}
+					className={classes.singUp}
+					variant="contained"
+					color="secondary"
+				>
 					{"Sure, I'm in"}
 				</Button>
 			</Box>
-
-			{/* <Box mb={3}>
-				<Typography align="center" variant="h5" color="textPrimary">
-					{name}
-				</Typography>
-			</Box>
-
-			{comingSoon ? (
-				<Box>
-					<SvgIcon className={classes.comingSoon} color="primary">
-						<ComingSoonImg
-							width="100%"
-							height="100%"
-							// width={160}
-						/>
-					</SvgIcon>
-				</Box>
-			) : (
-				<Box>
-					<CardRow
-						color="text.main"
-						fontWeight={"fontWeightRegular"}
-						fontSize={14}
-						text={"Total Staked"}
-						infoText={"Total Staked"}
-						justify="center"
-					/>
-
-					<CardRow
-						color="warning.main"
-						fontWeight={"fontWeightBold"}
-						fontSize={20}
-						text={totalStakedADX}
-						isAmountText
-						// infoText={totalStakedADX}
-						justify="center"
-					/>
-
-					<CardRow
-						color="text.main"
-						fontWeight={"fontWeightBold"}
-						fontSize={14}
-						text={totalStakedUSD}
-						isAmountText
-						// infoText={totalStakedADX}
-						justify="center"
-						mb={3}
-					/>
-
-					<CardRow
-						color="text.primary"
-						fontWeight={"fontWeightRegular"}
-						fontSize={14}
-						text={"Current annual yield (APY)"}
-						// infoText={"Current annual yield (APY)"}
-						justify="center"
-					/>
-
-					<CardRow
-						color="warning.main"
-						fontWeight={"fontWeightBold"}
-						fontSize={20}
-						text={currentAPY}
-						isAmountText
-						// infoText={currentAPY}
-						justify="center"
-					/>
-
-					<CardRow
-						color="text.main"
-						fontWeight={"fontWeightBold"}
-						fontSize={14}
-						text={`Weekly yield ${weeklyYield}`}
-						infoText={weeklyYieldInfo}
-						justify="center"
-						mb={3}
-					/>
-
-					<CardRow
-						color="text.main"
-						fontWeight={"fontWeightRegular"}
-						fontSize={14}
-						text={lockupPeriodTitle}
-						infoText={lockupPeriodInfo}
-						// infoText={"Current annual yield (APY)"}
-						justify="center"
-					/>
-
-					<CardRow
-						color="text.primary"
-						fontWeight={"fontWeightRegular"}
-						fontSize={14}
-						text={lockupPeriod}
-						// infoText={"Current annual yield (APY)"}
-						justify="center"
-						mb={3}
-					/>
-
-					<Tooltip
-						disableFocusListener={!disabled}
-						disableHoverListener={!disabled}
-						disableTouchListener={!disabled}
-						title={disabled ? disabledInfo : ""}
-					>
-						<div>
-							<Button
-								id={`stake-pool-${poolId}`}
-								fullWidth
-								variant="contained"
-								disableElevation
-								color="secondary"
-								onClick={onStakeBtnClick}
-								disabled={disabled}
-							>
-								{"Stake"}
-							</Button>
-						</div>
-					</Tooltip>
-				</Box>
-			)}
-
-			{!!loading && (
-				<Box
-					classes={{ root: classes.overlay }}
-					display="flex"
-					flexDirection="column"
-					alignItems="center"
-					justifyContent="center"
-				>
-					<Typography align="center" component="div" variant="h3"></Typography>
-				</Box>
-			)}
-			<Box classes={{ root: classes.iconBox }}>
-				{icon || null}
-				{!!loading && (
-					<CircularProgress
-						classes={{ root: classes.loading }}
-						size={69}
-						color="secondary"
-					/>
-				)}
-			</Box>
-			*/}
 			<Box classes={{ root: classes.iconBox }}>
 				<EmailAwardsIcon />
 			</Box>
