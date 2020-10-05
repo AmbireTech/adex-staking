759ce6b74da4b4ad82ecc0f1e8ff65bb2483fd98
IvoPaunov <ivo.paunov@gmail.com>
2020-10-05 15:07:02 +0300

    add loyalty pool deposits actions

--- src/actions/deposits.js
+++ src/actions/deposits.js
@@ -1,10 +1,25 @@
 import { DEPOSIT_POOLS } from "../helpers/constants"
-import { onLoyaltyPoolDeposit } from "./loyaltyPoolActions"
+import {
+	onLoyaltyPoolDeposit,
+	onLoyaltyPoolWithdraw
+} from "./loyaltyPoolActions"
 
 export const getDepositPool = poolId => DEPOSIT_POOLS.find(x => x.id === poolId)
 
 export const getDepositActionByPoolId = poolId => {
 	if (poolId === DEPOSIT_POOLS[0].id) {
 		return onLoyaltyPoolDeposit
+	}
+}
+
+export const getWithdrawActionByPoolId = poolId => {
+	if (poolId === DEPOSIT_POOLS[0].id) {
+		return onLoyaltyPoolWithdraw
+	}
+}
+
+export const getPoolStatsByPoolId = (stats, poolId) => {
+	if (poolId === DEPOSIT_POOLS[0].id) {
+		return stats.loyaltyPoolStats
 	}
 }
