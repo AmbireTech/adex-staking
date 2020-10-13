944deaa2462f86ea4783f5ab6d8efb488dd6f3d8
IvoPaunov <ivo.paunov@gmail.com>
2020-10-13 16:09:07 +0300

    add pool action for rewards withdraw actions

--- src/actions/pools.js
+++ src/actions/pools.js
@@ -3,6 +3,7 @@ import {
 	onLoyaltyPoolDeposit,
 	onLoyaltyPoolWithdraw
 } from "./loyaltyPoolActions"
+import { claimRewards } from "./actions"
 
 export const getDepositPool = poolId => DEPOSIT_POOLS.find(x => x.id === poolId)
 
@@ -16,6 +17,9 @@ export const getWithdrawActionByPoolId = poolId => {
 	if (poolId === DEPOSIT_POOLS[0].id) {
 		return onLoyaltyPoolWithdraw
 	}
+	if (poolId === POOLS[0].id) {
+		return claimRewards
+	}
 }
 
 export const getPoolStatsByPoolId = (stats, poolId) => {
@@ -25,4 +29,34 @@ export const getPoolStatsByPoolId = (stats, poolId) => {
 	if (poolId === POOLS[0].id) {
 		return stats.tomPoolStats
 	}
+}
+
+export const getWithdrawActionBySelectedRewardChannels = (
+	rewards,
+	chosenWalletType,
+	stats
+) => {
+	const actions = Object.entries(
+		rewards.reduce((byPool, r) => {
+			const { poolId } = r
+			byPool[poolId] = [...(byPool[poolId] || []), r]
+			return byPool
+		}, {})
+	).map(([poolId, rwds]) => {
+		if (poolId === DEPOSIT_POOLS[0].id) {
+			return onLoyaltyPoolWithdraw.bind(
+				null,
+				stats,
+				chosenWalletType,
+				rwds[0].outstandingReward
+			)
+		}
+		if (poolId === POOLS[0].id) {
+			const rewardChannels = rwds.map(r => r.rewardChannel)
+			return claimRewards.bind(null, chosenWalletType, rewardChannels)
+		}
+		return () => {}
+	})
+
+	return actions
 }
