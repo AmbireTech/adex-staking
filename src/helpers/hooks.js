360a6a68044b0537a55c29b9462b5302bf799551
IvoPaunov <ivo.paunov@gmail.com>
2020-10-13 17:55:15 +0300

    fix ethhereum event hooks

--- src/helpers/hooks.js
+++ src/helpers/hooks.js
@@ -3,46 +3,48 @@ import { useWeb3React } from "@web3-react/core"
 import { injected } from "./connector"
 
 export function useInactiveListener(suppress) {
-	const { active, error, activate } = useWeb3React()
+	const { activate } = useWeb3React()
+
+	const handleConnect = () => {
+		console.log("Handling 'connect' event")
+		activate(injected)
+	}
+
+	const handleChainChanged = chainId => {
+		console.log("Handling 'chainChanged' event with payload", chainId)
+		activate(injected)
+	}
+
+	const handleAccountsChanged = accounts => {
+		console.log("Handling 'accountsChanged' event with payload KOR", accounts)
+		if (accounts.length > 0) {
+			window.location.reload()
+		}
+	}
+
+	const handleNetworkChanged = networkId => {
+		console.log("Handling 'networkChanged' event with payload", networkId)
+		activate(injected)
+	}
 
 	useEffect(() => {
 		const { ethereum } = window
-		if (ethereum && ethereum.on && !active && !error && !suppress) {
-			const handleConnect = () => {
-				console.log("Handling 'connect' event")
-				activate(injected)
-			}
 
-			const handleChainChanged = chainId => {
-				console.log("Handling 'chainChanged' event with payload", chainId)
-				activate(injected)
-			}
-
-			const handleAccountsChanged = accounts => {
-				console.log("Handling 'accountsChanged' event with payload", accounts)
-				if (accounts.length > 0) {
-					activate(injected)
-				}
-			}
-
-			const handleNetworkChanged = networkId => {
-				console.log("Handling 'networkChanged' event with payload", networkId)
-				activate(injected)
-			}
-
+		if (ethereum) {
 			ethereum.on("connect", handleConnect)
 			ethereum.on("chainChanged", handleChainChanged)
 			ethereum.on("accountsChanged", handleAccountsChanged)
 			ethereum.on("networkChanged", handleNetworkChanged)
+		}
 
-			return () => {
-				if (ethereum.removeListener) {
-					ethereum.removeListener("connect", handleConnect)
-					ethereum.removeListener("chainChanged", handleChainChanged)
-					ethereum.removeListener("accountsChanged", handleAccountsChanged)
-					ethereum.removeListener("networkChanged", handleNetworkChanged)
-				}
+		return () => {
+			if (ethereum && ethereum.removeListener) {
+				ethereum.removeListener("connect", handleConnect)
+				ethereum.removeListener("chainChanged", handleChainChanged)
+				ethereum.removeListener("accountsChanged", handleAccountsChanged)
+				ethereum.removeListener("networkChanged", handleNetworkChanged)
 			}
 		}
-	}, [active, error, suppress, activate])
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 }
