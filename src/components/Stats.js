9ebf167def19dcb48ca040d54be959831d600612
IvoPaunov <ivo.paunov@gmail.com>
2020-11-02 17:16:05 +0200

    update stats chart for selectable chards and charts update

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import {
 	Box,
 	MenuItem,
@@ -7,13 +7,14 @@ import {
 	FormControl,
 	Grid
 } from "@material-ui/core"
-import { commify } from "ethers/utils"
 import ValidatorStatsContext from "../ValidatorStatsContext"
 import { POOLS } from "../helpers/constants"
 import { useTranslation } from "react-i18next"
 import { PropRow } from "./cardCommon"
 import StatsCard from "./StatsCard"
-import { formatADXPretty } from "../helpers/formatting"
+import { StatsChart } from "./StatsChart"
+import { formatADXPretty, formatNumberPretty } from "../helpers/formatting"
+import { PRIMARY } from "../themeMUi"
 
 const poolsSrc = POOLS.filter(x => x.selectable).map(x => ({
 	value: x.id,
@@ -21,24 +22,51 @@ const poolsSrc = POOLS.filter(x => x.selectable).map(x => ({
 	pool: x
 }))
 
-const ValidatorStatsCard = ({ label, value, loaded }) => (
-	<Box m={1} p={2} bgcolor="background.darkerPaper" boxShadow={25}>
-		<Box m={1}>
-			{StatsCard({
-				loaded: loaded,
-				title: label,
-				subtitle: value
-			})}
+const ValidatorStatsCard = ({
+	label,
+	value,
+	loaded,
+	currentKey,
+	chartKey,
+	setKey
+}) => {
+	const active = chartKey && currentKey && currentKey === chartKey
+	return (
+		<Box
+			m={1}
+			p={2}
+			bgcolor={active ? "background.paper" : "background.darkerPaper"}
+			boxShadow={25}
+			onClick={() => chartKey && setKey(chartKey)}
+		>
+			<Box m={1}>
+				{StatsCard({
+					loaded: loaded,
+					title: label,
+					subtitle: value
+				})}
+			</Box>
 		</Box>
-	</Box>
-)
+	)
+}
+
+const getDefaultLabels = (labels = []) => [
+	labels[0] || "",
+	labels[labels.length - 1] || ""
+]
 
 export default function Stats() {
 	const { t } = useTranslation()
+	const [chartDataKey, setChartDataKey] = useState("dailyPayoutsData")
+	const [chartData, setChartData] = useState({})
 
 	const { loaded, poolId, setPoolId, pool, setPool, stats } = useContext(
 		ValidatorStatsContext
 	)
+
+	useEffect(() => {
+		setChartData(stats[chartDataKey] || {})
+	}, [chartDataKey, stats])
 
 	return (
 		<Box>
@@ -67,8 +95,19 @@ export default function Stats() {
 			<Box>
 				<Grid container spacing={2}>
 					<Grid item md={12} lg={7}>
-						<Box width={1} height={420} bgcolor="background.special">
-							{"GRAPH HERE"}
+						<Box width={1} p={1} bgcolor="background.special">
+							<StatsChart
+								options={{
+									title: t(`stats.${chartDataKey}`)
+								}}
+								defaultLabels={getDefaultLabels(chartData.labels)}
+								data={chartData}
+								dataActive={true}
+								dataSynced={loaded}
+								yLabel={t(chartData.valueLabel)}
+								yColor={PRIMARY}
+								currency={chartData.currency}
+							/>
 						</Box>
 					</Grid>
 					<Grid item md={12} lg={5}>
@@ -125,13 +164,21 @@ export default function Stats() {
 							: "-"
 					}
 					loaded={loaded}
+					currentKey={chartDataKey}
+					chartKey={"dailyPayoutsData"}
+					setKey={setChartDataKey}
 				/>
 				<ValidatorStatsCard
 					label={t("stats.yearlyTransactions")}
 					value={
-						stats.yearlyTransactions ? commify(stats.yearlyTransactions) : "-"
+						stats.yearlyTransactions
+							? formatNumberPretty(stats.yearlyTransactions)
+							: "-"
 					}
 					loaded={loaded}
+					currentKey={chartDataKey}
+					chartKey={"yearlyTransactionsData"}
+					setKey={setChartDataKey}
 				/>
 			</Box>
 		</Box>
