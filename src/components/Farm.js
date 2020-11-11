acba2c3bde14afe62baa8855ee03452ac2b4efc5
IvoPaunov <ivo.paunov@gmail.com>
2020-11-11 16:07:19 +0200

    update farm page and translatins

--- src/components/Farm.js
+++ src/components/Farm.js
@@ -14,7 +14,7 @@ const Farm = () => {
 	const { t } = useTranslation()
 	const { stats, chosenWalletType, prices } = useContext(AppContext)
 
-	const canStake = !!chosenWalletType.name && !!stats.connectedWalletAddress
+	const canStake = true // !!chosenWalletType.name && !!stats.connectedWalletAddress
 
 	const justifyCenter = useMediaQuery(theme => theme.breakpoints.down("xs"))
 
@@ -43,16 +43,21 @@ const Farm = () => {
 								<YUSDIcon width="99%" height="99%" />
 							</SvgIcon>
 						]}
-						name={"BPT-ADX-yUSD"}
+						name={"Balancer ADX-yUSD"}
 						platform={"Balancer"}
-						depositAssets={"BPT (ADX-yUSD)"}
+						depositAssets={"BPT-ADX-yUSD"}
+						getDepositAssetsUrl={
+							"https://pools.balancer.exchange/#/pool/0x415900c6e18b89531e3e24c902b05c031c71a925/"
+						}
 						depositAssetsLink={
 							"https://pools.balancer.exchange/#/pool/0x415900c6e18b89531e3e24c902b05c031c71a925/"
 						}
 						rewardAssets={"ADX"}
-						totalDeposits={`${formatADXPretty(
-							stats.totalStakeTom
-						)} ADX; ${getADXInUSDFormatted(prices, stats.totalStakeTom)} yUSD`}
+						totalDepositTokenBalance={`${formatADXPretty(stats.totalStakeTom)}`}
+						totalDepositTokenStaked={`${formatADXPretty(
+							stats.totalStakeTom.div(2)
+						)}`}
+						userStakedShare={"N/A"}
 						currentAPY={`${50} %`}
 						weeklyYield={`${(50 / (365 / 7)).toFixed(4)} %`}
 						weeklyYieldInfo={[
@@ -62,16 +67,12 @@ const Farm = () => {
 						]}
 						onDepositBtnClick={() => {}}
 						onWithdrawBtnClick={() => {}}
-						loading={!stats.loaded}
+						loading={false && !stats.loaded}
 						disabled={!canStake}
 						disabledInfo={t("pools.connectWalletToStake")}
 						liquidityInfoText={t("farm.liquidityInfo")}
-						liquidityStaked={t("farm.staked", {
-							staked: "0.5 BPT = 20 ETH + 52100 ADX"
-						})}
-						liquidityOnWallet={t("farm.onWallet", {
-							onWallet: "1 BPT = 40 ETH + 104200 ADX"
-						})}
+						liquidityStaked={"1000.69 BPT-ADX-yUSD = 700 ADX + 60 yUSD "}
+						liquidityOnWallet={"420.69 BPT-ADX-yUSD = 300 ADX + 25 yUSD"}
 					/>
 				</Box>
 			</Box>
