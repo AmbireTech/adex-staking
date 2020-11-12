e72efbdab20df2842ed3199b435a756a5871e1bc
IvoPaunov <ivo.paunov@gmail.com>
2020-11-12 17:39:41 +0200

    update farm actions

--- src/actions/farm.js
+++ src/actions/farm.js
@@ -1,22 +1,32 @@
 import { Contract } from "ethers"
-import { FARM_TOKENS } from "../helpers/constants"
+import { FARM_POOLS, ZERO } from "../helpers/constants"
 import ERC20ABI from "../abi/ERC20"
+import MasterChefABI from "../abi/MasterChef"
 import { getSigner, defaultProvider } from "../ethereum"
 import { getUserIdentity } from "../helpers/identity"
 
-const getUserBalances = async ({ tokenContract, signer }) => {
-	if (!signer) {
+const MASTER_CHEF_ADDR = "0x2f0e755e0007E6569379a43E453F264b91336379"
+
+const MasterChef = new Contract(
+	MASTER_CHEF_ADDR,
+	MasterChefABI,
+	defaultProvider
+)
+
+const getUserBalances = async ({
+	depositTokenContract,
+	walletAddr,
+	identityAddr
+}) => {
+	if (!walletAddr) {
 		return {
 			identityBalance: null,
 			walletBalance: null
 		}
 	} else {
-		const walletAddr = await signer.getAddress()
-		const identityAddr = getUserIdentity(walletAddr).addr
-
 		const [identityBalance, walletBalance] = await Promise.all([
-			tokenContract.balanceOf(identityAddr),
-			tokenContract.balanceOf(walletAddr)
+			depositTokenContract.balanceOf(identityAddr),
+			depositTokenContract.balanceOf(walletAddr)
 		])
 
 		return {
@@ -26,33 +36,75 @@ const getUserBalances = async ({ tokenContract, signer }) => {
 	}
 }
 
-const getTokenStats = async ({ token, signer }) => {
-	const tokenContract = new Contract(
-		token.depositAssetsAddr,
+const getPoolStats = async ({ pool, walletAddr, identityAddr }) => {
+	const depositTokenContract = new Contract(
+		pool.depositAssetsAddr,
 		ERC20ABI,
 		defaultProvider
 	)
 
-	const [totalSupply, { identityBalance, walletBalance }] = await Promise.all([
-		tokenContract.totalSupply(),
-		getUserBalances({ tokenContract, signer })
+	const [
+		totalSupply,
+		totalStaked,
+		{ identityBalance, walletBalance },
+		pendingADX,
+		userInfo,
+		poolInfo
+	] = await Promise.all([
+		depositTokenContract.totalSupply(),
+		depositTokenContract.balanceOf(MasterChef.address),
+		getUserBalances({ depositTokenContract, walletAddr, identityAddr }),
+		identityAddr ? MasterChef.pendingADX(pool.poolId, walletAddr) : null,
+		identityAddr ? MasterChef.userInfo(pool.poolId, walletAddr) : null,
+		identityAddr ? MasterChef.poolInfo(pool.poolId) : null
 	])
+	const precision = 10_000_000
+
+	const userLPBalance = userInfo ? userInfo[0] : null
+
+	const useShare =
+		userLPBalance && totalStaked.gt(ZERO)
+			? userLPBalance
+					.mul(precision)
+					.div(totalStaked)
+					.toNumber() / precision
+			: null
 
 	return {
+		poolId: pool.poolId,
 		totalSupply,
+		totalStaked,
 		identityBalance,
-		walletBalance
+		walletBalance,
+		pendingADX,
+		userInfo,
+		userLPBalance,
+		useShare,
+		poolInfo
 	}
 }
 
-export const getFarmTokensStats = async ({ chosenWalletType }) => {
+export const getFarmPoolsStats = async ({ chosenWalletType }) => {
 	const signer =
 		chosenWalletType && chosenWalletType.library
 			? await getSigner(chosenWalletType)
 			: null
 
-	const tokenCalls = FARM_TOKENS.map(token => getTokenStats({ token, signer }))
-	const allTokensStats = await Promise.all(tokenCalls)
+	const walletAddr = signer ? await signer.getAddress() : null
+	const identityAddr = walletAddr ? getUserIdentity(walletAddr).addr : null
 
-	return allTokensStats
+	const poolsCalls = FARM_POOLS.map(pool =>
+		getPoolStats({ pool, walletAddr, identityAddr })
+	)
+	const allPoolStats = await Promise.all(poolsCalls)
+	const statsByPoolId = allPoolStats.reduce((byId, stats) => {
+		byId[stats.poolId] = stats
+		return byId
+	}, {})
+
+	return {
+		pollStatsLoaded: true,
+		userStatsLoaded: !!signer,
+		statsByPoolId
+	}
 }
