86e07a630350c6ced493c708543fd0d34ba8697d
IvoPaunov <ivo.paunov@gmail.com>
2020-11-18 13:33:04 +0200

    add farm stats  interval refresh

--- src/FarmProvider.js
+++ src/FarmProvider.js
@@ -1,12 +1,47 @@
-import React, { useEffect, useState, useContext } from "react"
+import React, { useEffect, useState, useContext, useCallback } from "react"
 import { getFarmPoolsStats } from "./actions/farm"
 import AppContext from "./AppContext"
+import { useTranslation } from "react-i18next"
 
 export const FarmContext = React.createContext()
 
+const REFRESH_INTERVAL = 69_000 // 69 sec
+
 function useFarm() {
-	const { chosenWalletType, prices } = useContext(AppContext)
+	const { t } = useTranslation()
+	const { chosenWalletType, prices, addSnack } = useContext(AppContext)
 	const [farmStats, setStats] = useState({})
+
+	const refreshFarmStats = useCallback(async () => {
+		if (Object.keys(prices).length) {
+			try {
+				const stats = await getFarmPoolsStats({
+					chosenWalletType,
+					externalPrices: prices
+				})
+				setStats(stats)
+			} catch (e) {
+				console.error("err loading farm stats", e)
+				if (e.code === 4001) {
+					addSnack(t("errors.authDeniedByUser"), "error", 20_000)
+				} else {
+					addSnack(
+						t("errors.loadingStats", {
+							error: !!e ? e.message || e.toString() : ""
+						}),
+						"error",
+						20_000
+					)
+				}
+			}
+		}
+	}, [addSnack, chosenWalletType, prices, t])
+
+	useEffect(() => {
+		const intvl = setInterval(refreshFarmStats, REFRESH_INTERVAL)
+		return () => clearInterval(intvl)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [refreshFarmStats])
 
 	useEffect(() => {
 		// TODO: update on wallet change but not on prices change
