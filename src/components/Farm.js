3843b8f12527976ee0bb2abed31fbd171359004e
IvoPaunov <ivo.paunov@gmail.com>
2020-11-20 15:58:53 +0200

    add harvest btn and dialog to farm page

--- src/components/Farm.js
+++ src/components/Farm.js
@@ -1,26 +1,34 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { FarmContext } from "../FarmProvider"
 import AppContext from "../AppContext"
-import { Box, useMediaQuery } from "@material-ui/core"
+import { Box, useMediaQuery, Button } from "@material-ui/core"
 import FarmCard from "./FarmCard"
 import FarmInfoCard from "./FarmInfoCard"
 import SectionHeader from "./SectionHeader"
-import { useTranslation } from "react-i18next"
-import { FARM_POOLS } from "../helpers/constants"
+import { useTranslation, Trans } from "react-i18next"
+import { FARM_POOLS, ZERO } from "../helpers/constants"
+import ConfirmationDialog from "./ConfirmationDialog"
+import { onHarvestAll } from "../actions"
+import {
+	formatADXPretty
+	// formatTokens
+} from "../helpers/formatting"
 
 const Farm = () => {
 	const { t } = useTranslation()
 	const { farmStats, setGetFarmStats } = useContext(FarmContext)
-	const { chosenWalletType } = useContext(AppContext)
+	const { chosenWalletType, wrapDoingTxns } = useContext(AppContext)
 	const {
 		pollStatsLoaded,
 		userStatsLoaded,
 		statsByPoolId,
-		blockNumber
+		blockNumber,
+		totalRewards
 	} = farmStats
 
 	const canStake = !!chosenWalletType.name && userStatsLoaded //&& !!appStats.connectedWalletAddress
 	const justifyCenter = useMediaQuery(theme => theme.breakpoints.down("xs"))
+	const [harvestOpen, setHarvestOpen] = useState(false)
 
 	useEffect(() => {
 		setGetFarmStats(true)
@@ -30,9 +38,31 @@ const Farm = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
+	const onHarvest = async () => {
+		await wrapDoingTxns(
+			onHarvestAll.bind(null, {
+				farmStats,
+				chosenWalletType
+			})
+		)()
+	}
+
 	return (
 		<Box>
-			<SectionHeader title={t("common.farm")} actions={<></>} />
+			<SectionHeader
+				title={t("common.farm")}
+				actions={
+					<Button
+						id="btn-rewards-page-re-stake"
+						variant="contained"
+						color="secondary"
+						onClick={() => setHarvestOpen(true)}
+						disabled={!userStatsLoaded || !totalRewards}
+					>
+						{t("farm.harvest")}
+					</Button>
+				}
+			/>
 			<Box mt={2}>
 				<Box
 					display="flex"
@@ -62,6 +92,27 @@ const Farm = () => {
 					})}
 				</Box>
 			</Box>
+			{ConfirmationDialog({
+				isOpen: harvestOpen,
+				onDeny: () => setHarvestOpen(false),
+				onConfirm: () => {
+					setHarvestOpen(false)
+					onHarvest()
+				},
+				confirmActionName: t("common.harvest"),
+				content: (
+					<Trans
+						i18nKey="dialogs.harvestAllConfirmation"
+						values={{
+							amount: formatADXPretty(totalRewards || ZERO),
+							currency: "ADX"
+						}}
+						components={{
+							box: <Box mb={2}></Box>
+						}}
+					/>
+				)
+			})}
 		</Box>
 	)
 }
