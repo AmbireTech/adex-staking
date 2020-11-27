8f72a5f475b7edb24491ddda9f03e3037175a3a1
IvoPaunov <ivo.paunov@gmail.com>
2020-11-27 18:48:27 +0200

    pssible fix of wss connection timeout

--- src/ethereum.js
+++ src/ethereum.js
@@ -6,7 +6,13 @@ const LocalProvider = REACT_APP_RPC_URL.startsWith("wss://")
 	? providers.WebSocketProvider
 	: providers.JsonRpcProvider
 
-export const defaultProvider = new LocalProvider(REACT_APP_RPC_URL, "homestead")
+export let defaultProvider = new LocalProvider(REACT_APP_RPC_URL, "homestead")
+
+// TODO: add get defaultProvider func
+defaultProvider.on("error", e => {
+	console.error("WS Error", e)
+	defaultProvider = new LocalProvider(REACT_APP_RPC_URL, "homestead")
+})
 
 export async function getSigner(chosenWalletType) {
 	// console.log("chosenWalletType", chosenWalletType)
