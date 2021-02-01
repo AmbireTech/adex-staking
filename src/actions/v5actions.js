8062ea51064d5737ab74ee5f640ae6c710034ede
IvoPaunov <ivo.paunov@gmail.com>
2021-02-01 18:10:35 +0200

    init v5 actions

---
+++ src/actions/v5actions.js
@@ -0,0 +1,7 @@
+export async function onMigrationToV5(
+	chosenWalletType,
+	{ amount, poolId, nonce }
+) {
+	// TODO: waiting for migration contract
+	console.log(chosenWalletType, amount, poolId, nonce)
+}
