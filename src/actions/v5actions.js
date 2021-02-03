0abff187fd855373b02e498b99a07a75576dbda4
IvoPaunov <ivo.paunov@gmail.com>
2021-02-03 15:38:48 +0200

    wotk on stakin pool stats data

--- src/actions/v5actions.js
+++ src/actions/v5actions.js
@@ -1,7 +1,58 @@
+import { Contract } from "ethers"
+import ERC20ABI from "../abi/ERC20"
+import ADXTokenABI from "../abi/ADXToken"
+import ADXSupplyControllerABI from "../abi/ADXSupplyController"
+import {
+	ADDR_ADX,
+	ADDR_ADX_LOYALTY_TOKEN,
+	ZERO,
+	MAX_UINT,
+	DEPOSIT_POOLS
+} from "../helpers/constants"
+import { getSigner, getDefaultProvider } from "../ethereum"
+
+const ADDR_STAKING_POOL = "0x0000000000000000000000000000000000000000"
+const ADDR_ADX_SUPPLY_CONTROLLER = "0x617e6f354d288fcb33e148b1bb6d2cc9be1f7695"
+const stakingPoolABI = ERC20ABI //TODO
+const supplyControllerABI = ADXSupplyControllerABI
+const secondsInYear = 60 * 60 * 24 * 365
+const PRECISION = 1_000_000
+
+const provider = getDefaultProvider
+
+const Token = new Contract(ADDR_ADX, ADXTokenABI, provider)
+const ADXSupplyController = new Contract(
+	ADDR_ADX_SUPPLY_CONTROLLER,
+	supplyControllerABI,
+	provider
+)
+// const StakingPool = new Contract(ADDR_STAKING_POOL, stakingPoolABI, provider)
+
 export async function onMigrationToV5(
 	chosenWalletType,
 	{ amount, poolId, nonce }
 ) {
 	// TODO: waiting for migration contract
 	console.log(chosenWalletType, amount, poolId, nonce)
+}
+
+export async function getStakingPortalData() {
+	const [poolTotalStaked, incentivePerSecond] = await Promise.all([
+		Token.balanceOf(ADDR_ADX_LOYALTY_TOKEN),
+		ADXSupplyController.incentivePerSecond(ADDR_STAKING_POOL)
+	])
+
+	return {
+		poolTotalStaked,
+		incentivePerSecond,
+		currentAPY: incentivePerSecond.isZero()
+			? 0
+			: (incentivePerSecond
+					.mul(PRECISION)
+					.mul(secondsInYear)
+					.div(incentivePerSecond)
+					.toNumber() /
+					PRECISION) *
+			  100
+	}
 }
