968167f97a5077b578ea67cd8a598fda281801a6
IvoPaunov <ivo.paunov@gmail.com>
2021-02-03 18:53:37 +0200

    work on v5 stakings data

--- src/actions/v5actions.js
+++ src/actions/v5actions.js
@@ -2,15 +2,11 @@ import { Contract } from "ethers"
 import ERC20ABI from "../abi/ERC20"
 import ADXTokenABI from "../abi/ADXToken"
 import ADXSupplyControllerABI from "../abi/ADXSupplyController"
-import {
-	ADDR_ADX,
-	ADDR_ADX_LOYALTY_TOKEN,
-	ZERO,
-	MAX_UINT,
-	DEPOSIT_POOLS
-} from "../helpers/constants"
-import { getSigner, getDefaultProvider } from "../ethereum"
+import { ADDR_ADX, ADDR_ADX_LOYALTY_TOKEN } from "../helpers/constants"
+import { getDefaultProvider } from "../ethereum"
+import { getUserIdentity } from "../helpers/identity"
 
+const ZERO_ADDR = "0x0000000000000000000000000000000000000000"
 const ADDR_STAKING_POOL = "0x0000000000000000000000000000000000000000"
 const ADDR_ADX_SUPPLY_CONTROLLER = "0x617e6f354d288fcb33e148b1bb6d2cc9be1f7695"
 const stakingPoolABI = ERC20ABI //TODO
@@ -26,7 +22,7 @@ const ADXSupplyController = new Contract(
 	supplyControllerABI,
 	provider
 )
-// const StakingPool = new Contract(ADDR_STAKING_POOL, stakingPoolABI, provider)
+const StakingPool = new Contract(ADDR_STAKING_POOL, stakingPoolABI, provider)
 
 export async function onMigrationToV5(
 	chosenWalletType,
@@ -54,5 +50,81 @@ export async function getStakingPortalData() {
 					.toNumber() /
 					PRECISION) *
 			  100
+	}
+}
+
+export async function getStakings({ walletAddr, prices }) {
+	const identityAddr = getUserIdentity(walletAddr).addr
+
+	const [
+		balanceSPToken,
+		currentShareValue,
+		enterLogs,
+		leaveLogs,
+		adexTokenTransfersLogs,
+		burnLogs // To detect rage leave
+	] = await Promise.all([
+		StakingPool.balanceOf(identityAddr),
+		StakingPool.shareValue(),
+		provider.getLogs({
+			fromBlock: 0,
+			...StakingPool.filters.Transfer(null, identityAddr, null)
+		}),
+		provider.getLogs({
+			fromBlock: 0,
+			...StakingPool.filters.LogLeave(walletAddr, ADDR_ADX_LOYALTY_TOKEN, null)
+		}),
+		provider.getLogs({
+			fromBlock: 0,
+			...Token.filters.Transfer(ADDR_STAKING_POOL, identityAddr, null)
+		}),
+		provider.getLogs({
+			fromBlock: 0,
+			...StakingPool.filters.Transfer(identityAddr, ZERO_ADDR, null)
+		})
+	])
+
+	const balanceSPADX = balanceSPToken.mul(currentShareValue)
+
+	const userEnters = enterLogs.msp(log => {
+		const parsedLog = StakingPool.interface.parseLog(log)
+
+		return {
+			amount: parsedLog[2]
+			// time: //TODO
+		}
+	})
+
+	const userLeaves = leaveLogs.map(log => {
+		const parsedLog = StakingPool.interface.parseLog(log)
+
+		return {
+			willUnlockAt: parsedLog[1],
+			adxAmount: parsedLog[2]
+			// time: //TODO
+		}
+	})
+
+	const burnsByTxHash = burnLogs.reduce((txns, log) => {
+		txns[log.transactionHash] = log
+		return txns
+	}, {})
+
+	const userWithdraws = adexTokenTransfersLogs.map(log => {
+		const burnTxLog = burnsByTxHash[log.transactionHash]
+		const parsedADXTransferLog = Token.interface.parseLog(log)
+
+		return {
+			amount: parsedADXTransferLog[1],
+			isRageLeave: !!burnTxLog
+			// time: //TODO
+		}
+	})
+
+	return {
+		balanceSPADX,
+		userEnters,
+		userLeaves,
+		userWithdraws
 	}
 }
