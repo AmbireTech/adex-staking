f3f5982ea4afabfd30400eed14119fa710542b21
IvoPaunov <ivo.paunov@gmail.com>
2021-02-04 18:37:00 +0200

    init staking pool v5 component

---
+++ src/components/StakingPoolV5.js
@@ -0,0 +1,68 @@
+import React, { useContext } from "react"
+import {
+	TableRow,
+	TableCell,
+	Box,
+	Table,
+	TableContainer,
+	TableHead,
+	TableBody
+} from "@material-ui/core"
+import { formatADXPretty } from "../helpers/formatting"
+import AppContext from "../AppContext"
+import { AmountText } from "./cardCommon"
+import { useTranslation } from "react-i18next"
+
+export default function Deposits() {
+	const { t } = useTranslation()
+	const { stats } = useContext(AppContext)
+	const { tomStakingV5PoolStats } = stats
+	const { stakings } = tomStakingV5PoolStats
+
+	const renderStakingEventRow = stakingEvent => {
+		return (
+			<TableRow key={stakingEvent.blockNumber + stakingEvent.type}>
+				<TableCell>
+					<Box
+						display="flex"
+						flexDirection="row"
+						alignItems="center"
+						justifyContent="flex-start"
+					>
+						<Box>{stakingEvent.label}</Box>
+					</Box>
+				</TableCell>
+				<TableCell align="right">{stakingEvent.type}</TableCell>
+				<TableCell align="right">
+					<AmountText
+						text={`${formatADXPretty(stakingEvent.amount)} ${"ADX-LOYALTY"}`}
+						fontSize={17}
+					/>
+				</TableCell>
+				<TableCell align="right">{stakingEvent.blockNumber}</TableCell>
+			</TableRow>
+		)
+	}
+
+	return (
+		<Box>
+			<Box>
+				<TableContainer xs={12}>
+					<Table aria-label="Bonds table">
+						<TableHead>
+							<TableRow>
+								<TableCell>{t("common.pool")}</TableCell>
+								<TableCell align="right">{t("common.type")}</TableCell>
+								<TableCell align="right">{t("common.amount")}</TableCell>
+								<TableCell align="right">{t("common.blockNumber")}</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{[...(stakings || [])].map(renderStakingEventRow)}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			</Box>
+		</Box>
+	)
+}
