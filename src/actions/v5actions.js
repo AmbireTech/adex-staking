2aa0dc46e6294f14b6c3d5bec94589910c526220
IvoPaunov <ivo.paunov@gmail.com>
2021-02-10 18:17:13 +0200

    update stakin v5 event logs

--- src/actions/v5actions.js
+++ src/actions/v5actions.js
@@ -91,76 +91,76 @@ export async function getTomStakingV5PoolData() {
 /*
 export async function loadUserTomStakingV5PoolStats({ identityAddr } = {}) {
 
-    const poolData = await getTomStakingV5PoolData()
-    if (!identityAddr) {
-        return {
-            ...STAKING_POOL_EMPTY_STATS,
-            ...poolData,
-            loaded: true
-        }
-    }
+	const poolData = await getTomStakingV5PoolData()
+	if (!identityAddr) {
+		return {
+			...STAKING_POOL_EMPTY_STATS,
+			...poolData,
+			loaded: true
+		}
+	}
 
-    const decimalsString = '000000000000000000'
+	const decimalsString = '000000000000000000'
 
-    const stakings = [
-        {
-            label: 'Tom Staking Pool V5',
-            type: STAKING_POOL_EVENT_TYPES.enter,
-            amount: BigNumber.from(2000 + decimalsString),
-            blockNumber: 11295886,
-			transactionHash: 1
-        },
-        {
-            label: 'Tom Staking Pool V5',
-            type: STAKING_POOL_EVENT_TYPES.leave,
-			withdrawTxHash: 4,
-            amount: BigNumber.from(420 + decimalsString),
-            blockNumber: 11482093,
-			transactionHash: 2
-        },
+	const stakings = [
 		{
-            label: 'Tom Staking Pool V5',
-            type: STAKING_POOL_EVENT_TYPES.leave,
-            amount: BigNumber.from(500 + decimalsString),
-            blockNumber: 11482999,
+			label: 'Tom Staking Pool V5',
+			type: STAKING_POOL_EVENT_TYPES.enter,
+			amount: BigNumber.from(2000 + decimalsString),
+			blockNumber: 11295886,
+			transactionHash: 1
+		},
+		{
+			label: 'Tom Staking Pool V5',
+			type: STAKING_POOL_EVENT_TYPES.leave,
+			withdrawTxHash: 4,
+			amount: BigNumber.from(420 + decimalsString),
+			blockNumber: 11482093,
+			transactionHash: 2
+		},
+		{
+			label: 'Tom Staking Pool V5',
+			type: STAKING_POOL_EVENT_TYPES.leave,
+			amount: BigNumber.from(500 + decimalsString),
+			blockNumber: 11482999,
 			transactionHash: 3
-        },
-        {
-            label: 'Tom Staking Pool V5',
-            type: STAKING_POOL_EVENT_TYPES.withdraw,
-            amount: BigNumber.from(420 + decimalsString),
-            blockNumber: 11661741,
+		},
+		{
+			label: 'Tom Staking Pool V5',
+			type: STAKING_POOL_EVENT_TYPES.withdraw,
+			amount: BigNumber.from(420 + decimalsString),
+			blockNumber: 11661741,
 			transactionHash: 4
-        },
-        {
-            label: 'Tom Staking Pool V5',
-            type: STAKING_POOL_EVENT_TYPES.rageLeave,
-            amount: BigNumber.from(333 + decimalsString),
-            blockNumber: 11789046,
+		},
+		{
+			label: 'Tom Staking Pool V5',
+			type: STAKING_POOL_EVENT_TYPES.rageLeave,
+			amount: BigNumber.from(333 + decimalsString),
+			blockNumber: 11789046,
 			transactionHash: 5
-        },
-    ]
+		},
+	]
 
-    const withTimestamp = await Promise.all(
-        stakings.map(async (stakngEvent) => {
-            const { timestamp } = (await provider.getBlock(stakngEvent.blockNumber))
-            return {
-                ...stakngEvent,
-                timestamp: timestamp * 1000
-            }
+	const withTimestamp = await Promise.all(
+		stakings.map(async (stakngEvent) => {
+			const { timestamp } = (await provider.getBlock(stakngEvent.blockNumber))
+			return {
+				...stakngEvent,
+				timestamp: timestamp * 1000
+			}
 
-        })
-    )
+		})
+	)
 
-    const balanceSPADX = BigNumber.from(4 + decimalsString)
+	const balanceSPADX = BigNumber.from(4 + decimalsString)
 
-    return {
-        ...poolData,
-        balanceSPADX,
-        stakings: withTimestamp,
+	return {
+		...poolData,
+		balanceSPADX,
+		stakings: withTimestamp,
 		loaded: true,
-        userDataLoaded: true
-    }
+		userDataLoaded: true
+	}
 }
 */
 
@@ -177,10 +177,11 @@ export async function loadUserTomStakingV5PoolStats({ identityAddr } = {}) {
 	const [
 		balanceSPToken,
 		currentShareValue,
-		enterLogs,
+		mintLogs,
+		enterTransferLogs,
 		leaveLogs,
-		adexTokenTransfersLogs,
-		burnLogs // To detect rage leave
+		withdrawLogs,
+		rageLeaveLogs
 	] = await Promise.all([
 		StakingPool.balanceOf(identityAddr),
 		StakingPool.shareValue(),
@@ -190,64 +191,105 @@ export async function loadUserTomStakingV5PoolStats({ identityAddr } = {}) {
 		}),
 		provider.getLogs({
 			fromBlock: 0,
-			...StakingPool.filters.LogLeave(identityAddr, ADDR_STAKING_POOL, null)
+			...Token.filters.Transfer(identityAddr, ADDR_STAKING_POOL, null)
 		}),
 		provider.getLogs({
 			fromBlock: 0,
-			...Token.filters.Transfer(ADDR_STAKING_POOL, identityAddr, null)
+			...StakingPool.filters.LogLeave(identityAddr, null, null, null)
 		}),
 		provider.getLogs({
 			fromBlock: 0,
-			...StakingPool.filters.Transfer(identityAddr, ZERO_ADDR, null)
+			...StakingPool.filters.LogWithdraw(identityAddr, null, null, null)
+		}),
+		provider.getLogs({
+			fromBlock: 0,
+			...StakingPool.filters.LogRageLeave(identityAddr, null, null, null)
 		})
 	])
 
 	const balanceSPADX = balanceSPToken.mul(currentShareValue)
 
-	const userEnters = enterLogs.msp(log => {
-		const parsedLog = StakingPool.interface.parseLog(log)
-
-		return {
-			transactionHash: log.transactionHash,
-			type: STAKING_POOL_EVENT_TYPES.enter,
-			amount: parsedLog.args.amount, // [2]
-			blockNumber: log.blockNumber
-		}
-	})
-
-	const burnsByTxHash = burnLogs.reduce((txns, log) => {
+	const enterTransferByTxHash = enterTransferLogs.reduce((txns, log) => {
 		txns[log.transactionHash] = log
 		return txns
 	}, {})
 
-	const userWithdraws = adexTokenTransfersLogs.map(log => {
-		const burnTxLog = burnsByTxHash[log.transactionHash]
-		const parsedADXTransferLog = Token.interface.parseLog(log)
+	const userEnters = mintLogs
+		.msp(log => {
+			const adxTransferLog = enterTransferByTxHash[log.transactionHash]
+			const parsedADXTransferLog = adxTransferLog
+				? Token.interface.parseLog(log)
+				: null
+
+			if (parsedADXTransferLog) {
+				const parsedMintLog = StakingPool.interface.parseLog(log)
+				return {
+					transactionHash: log.transactionHash,
+					type: STAKING_POOL_EVENT_TYPES.enter,
+					shares: parsedMintLog.args.amount, // [2]
+					adxAmount: parsedADXTransferLog.args.amount, // [2]
+					blockNumber: log.blockNumber
+				}
+			} else {
+				return null
+			}
+		})
+		.filter(x => !!x)
+
+	const userWithdraws = withdrawLogs.map(log => {
+		const parsedWithdrawLog = StakingPool.interface.parseLog(log)
+		const {
+			shares,
+			unlocksAt,
+			maxTokens,
+			receivedTokens
+		} = parsedWithdrawLog.args
 
 		return {
 			transactionHash: log.transactionHash,
-			type: !!burnTxLog
-				? STAKING_POOL_EVENT_TYPES.rageLeave
-				: STAKING_POOL_EVENT_TYPES.withdraw,
-			amount: parsedADXTransferLog.args.amount, //[2]
+			type: STAKING_POOL_EVENT_TYPES.withdraw,
+			shares, //[1]
+			unlocksAt, //[2]
+			maxTokens, //[3]
+			receivedTokens, //[4]
+			blockNumber: log.blockNumber
+		}
+	})
+
+	const userRageLeaves = rageLeaveLogs.map(log => {
+		const parsedRageLeaveLog = StakingPool.interface.parseLog(log)
+
+		const { shares, adxAmount, receivedTokens } = parsedRageLeaveLog.args
+
+		return {
+			transactionHash: log.transactionHash,
+			type: STAKING_POOL_EVENT_TYPES.rageLeave,
+			shares, //[1]
+			adxAmount, //[2]
+			receivedTokens, //[3]
 			blockNumber: log.blockNumber
 		}
 	})
 
 	const userLeaves = leaveLogs.map(log => {
 		const parsedLog = StakingPool.interface.parseLog(log)
+		const { shares, unlocksAt, maxTokens } = parsedLog.args
 
 		return {
 			transactionHash: log.transactionHash,
 			type: STAKING_POOL_EVENT_TYPES.leave,
-			willUnlockAt: parsedLog.args.willUnlockAt, //[1]
-			adxAmount: parsedLog.args.adxAmount, // [2]
+			shares, // [1]
+			unlocksAt, //[2]
+			maxTokens, // [3]
 			blockNumber: log.blockNumber
 			//TODO: detect withdraw tx
 		}
 	})
 
-	const stakings = userEnters.concat(userLeaves).concat(userWithdraws)
+	const stakings = userEnters
+		.concat(userLeaves)
+		.concat(userWithdraws)
+		.concat(userRageLeaves)
 
 	const withTimestamp = await Promise.all(
 		stakings.map(async stakngEvent => {
