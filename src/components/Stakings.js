b31d2dd0d0f1a3e4ca742b18c5479b58a2cacd28
IvoPaunov <ivo.paunov@gmail.com>
2021-02-23 15:22:04 +0200

    add staking pool history to stakings page

--- src/components/Stakings.js
+++ src/components/Stakings.js
@@ -3,7 +3,7 @@ import { Box, Typography } from "@material-ui/core"
 import AppContext from "../AppContext"
 import Bonds from "./Bonds"
 import Deposits from "./Deposits"
-// import StakingPoolV5 from "./StakingPoolV5"
+import StakingPoolTxnsHistory from "./StakingPoolTxnsHistory"
 import SectionHeader from "./SectionHeader"
 import { useTranslation } from "react-i18next"
 
@@ -23,20 +23,6 @@ const Stakings = () => {
 	return (
 		<Box>
 			<SectionHeader title={t("common.staked")} />
-			{/* <Box mt={2}>
-				<Box color="text.main">
-					<Typography variant="h5" gutterBottom>
-						{t("common.stakings")}
-					</Typography>
-				</Box>
-				<Box mt={2} bgcolor="background.darkerPaper" boxShadow={25}>
-					<Box p={3}>
-						{StakingPoolV5({
-							stats
-						})}
-					</Box>
-				</Box>
-			</Box> */}
 			<Box mt={2}>
 				<Box color="text.main">
 					<Typography variant="h5" gutterBottom>
@@ -68,6 +54,20 @@ const Stakings = () => {
 				<Box mt={3} bgcolor="background.darkerPaper" boxShadow={25}>
 					<Box p={3}>
 						<Deposits />
+					</Box>
+				</Box>
+			</Box>
+			<Box mt={2}>
+				<Box color="text.main">
+					<Typography variant="h5" gutterBottom>
+						{t("common.txnsHistory")}
+					</Typography>
+				</Box>
+				<Box mt={2} bgcolor="background.darkerPaper" boxShadow={25}>
+					<Box p={3}>
+						{StakingPoolTxnsHistory({
+							stats
+						})}
 					</Box>
 				</Box>
 			</Box>
