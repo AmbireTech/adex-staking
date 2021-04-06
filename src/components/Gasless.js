e63ae786ce5968c537cb13ca1138a39fb9d82702
IvoPaunov <ivo.paunov@gmail.com>
2021-04-06 18:02:30 +0300

    work on updating gasless page

--- src/components/Gasless.js
+++ src/components/Gasless.js
@@ -1,92 +1,79 @@
-import React from // useContext,
-// useEffect,
-// useState
-"react"
-// import { makeStyles } from "@material-ui/core/styles"
+import React, { useContext, useEffect, useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
 import {
 	Box,
 	SvgIcon,
-	Typography
-	// IconButton,
-	// Button,
-	// Modal,
-	// Fade,
-	// Backdrop
+	Typography,
+	IconButton,
+	Button,
+	Modal,
+	Fade,
+	Backdrop
 } from "@material-ui/core"
-// import {
-// 	FileCopySharp as CopyIcon,
-// 	HelpSharp as HelpIcon
-// } from "@material-ui/icons"
-// import copy from "copy-to-clipboard"
+import {
+	FileCopySharp as CopyIcon,
+	HelpSharp as HelpIcon
+} from "@material-ui/icons"
+import copy from "copy-to-clipboard"
 import { ReactComponent as GaslessIcon } from "./../resources/gasless-ic.svg"
 import SectionHeader from "./SectionHeader"
-// import AppContext from "../AppContext"
-// import { createNewBond, restake, getGaslessInfo } from "../actions"
-// import {
-// 	POOLS,
-// 	MIN_BALANCE_FOR_GASLESS_TXNS,
-// 	UNBOND_DAYS,
-// 	ZERO
-// } from "../helpers/constants"
-// import StatsCard from "./StatsCard"
-// import { formatADXPretty } from "../helpers/formatting"
-// import NewGaslessBondForm from "./NewGaslessBondForm"
-// import { ExternalAnchor } from "./Anchor"
-// import Tooltip from "./Tooltip"
-// import ConfirmationDialog from "./ConfirmationDialog"
-import {
-	useTranslation
-	// Trans
-} from "react-i18next"
+import AppContext from "../AppContext"
+import { createNewBond, getGaslessInfo } from "../actions"
+import { MIN_BALANCE_FOR_GASLESS_TXNS } from "../helpers/constants"
+import StatsCard from "./StatsCard"
+import { formatADXPretty } from "../helpers/formatting"
+import NewGaslessBondForm from "./NewGaslessBondForm"
+import { ExternalAnchor } from "./Anchor"
+import Tooltip from "./Tooltip"
+import { useTranslation, Trans } from "react-i18next"
 
-// const MIN_GASLESS_RE_STAKE_REWARDS = MIN_BALANCE_FOR_GASLESS_TXNS.div(4)
+const MIN_GASLESS_RE_STAKE_REWARDS = MIN_BALANCE_FOR_GASLESS_TXNS.div(4)
 
-// const useStyles = makeStyles(theme => {
-// 	return {
-// 		overlay: {
-// 			position: "absolute",
-// 			left: 0,
-// 			top: 0,
-// 			right: 0,
-// 			bottom: 0,
-// 			backgroundColor: "transparent"
-// 		},
-// 		noUserData: {
-// 			opacity: 0.23
-// 		},
-// 		address: {
-// 			wordBreak: "break-all"
-// 		},
-// 		modal: {
-// 			display: "flex",
-// 			alignItems: "center",
-// 			justifyContent: "center"
-// 		},
-// 		bullets: {
-// 			[theme.breakpoints.up("md")]: {
-// 				maxWidth: 800
-// 			}
-// 		},
-// 		actions: {
-// 			maxWidth: 400,
-// 			margin: theme.spacing(1)
-// 		}
-// 	}
-// })
+const useStyles = makeStyles(theme => {
+	return {
+		overlay: {
+			position: "absolute",
+			left: 0,
+			top: 0,
+			right: 0,
+			bottom: 0,
+			backgroundColor: "transparent"
+		},
+		noUserData: {
+			opacity: 0.23
+		},
+		address: {
+			wordBreak: "break-all"
+		},
+		modal: {
+			display: "flex",
+			alignItems: "center",
+			justifyContent: "center"
+		},
+		bullets: {
+			[theme.breakpoints.up("md")]: {
+				maxWidth: 800
+			}
+		},
+		actions: {
+			maxWidth: 400,
+			margin: theme.spacing(1)
+		}
+	}
+})
 
-// const defaultGaslessInfo = {
-// 	canExecuteGasless: false,
-// 	canExecuteGaslessError: {
-// 		message: "common.connectWallet"
-// 	}
-// }
+const defaultGaslessInfo = {
+	canExecuteGasless: true,
+	canExecuteGaslessError: {
+		message: "common.connectWallet"
+	}
+}
 
 const Gasless = () => {
 	const { t } = useTranslation()
-	/*
+
 	const classes = useStyles()
 	const [bondOpen, setBondOpen] = useState(false)
-	const [reStakeOpen, setReStakeOpen] = useState(false)
 	const [bond, setBond] = useState({})
 	const [gaslessInfo, setGaslessInfo] = useState(defaultGaslessInfo)
 
@@ -99,60 +86,45 @@ const Gasless = () => {
 		account
 	} = useContext(AppContext)
 
+	const { tomStakingV5PoolStats } = stats
+
 	const {
-		userIdentityBalance,
-		identityAddr,
-		loaded,
-		tomRewardADX,
-		userBonds
-	} = stats
+		userDataLoaded,
+		gaslessAddress,
+		gaslessAddrBalance
+	} = tomStakingV5PoolStats
 
 	const {
 		canExecuteGasless,
 		canExecuteGaslessError: gaslessError
 	} = gaslessInfo
 
-	useEffect(() => {
-		async function updateGasless() {
-			console.log("account", account)
+	// useEffect(() => {
+	// 	async function updateGasless() {
+	// 		console.log("account", account)
 
-			const info = await getGaslessInfo(account)
-			setGaslessInfo(info)
-		}
+	// 		const info = await getGaslessInfo(account)
+	// 		setGaslessInfo(info)
+	// 	}
 
-		if (account) {
-			updateGasless()
-		} else {
-			setGaslessInfo(defaultGaslessInfo)
-		}
-	}, [account])
+	// 	if (account) {
+	// 		updateGasless()
+	// 	} else {
+	// 		setGaslessInfo(defaultGaslessInfo)
+	// 	}
+	// }, [account])
 
-	const hasEnoughForReStake = tomRewardADX.gte(MIN_GASLESS_RE_STAKE_REWARDS)
-
-	const walletConnected = identityAddr && loaded
+	const walletConnected = gaslessAddress && userDataLoaded
 
 	const mainErr = !walletConnected
 		? "common.connectWallet"
 		: !canExecuteGasless
-			? t(gaslessError.message || "", gaslessError.data || {})
-			: ""
-
-	const canExecuteGaslessReStakeError =
-		mainErr ||
-		(!hasEnoughForReStake
-			? t("errors.minGaslessReStake", {
-				amount: formatADXPretty(MIN_GASLESS_RE_STAKE_REWARDS),
-				currency: "ADX"
-			})
-			: "")
+		? t(gaslessError.message || "", gaslessError.data || {})
+		: ""
 
 	const canExecuteGaslessError =
-		mainErr || (userIdentityBalance.isZero() ? t("errors.nothingToStake") : "")
+		mainErr || (gaslessAddrBalance.isZero() ? t("errors.nothingToStake") : "")
 
-	const showReStake =
-		walletConnected &&
-		userBonds &&
-		userBonds.some(x => x.poolId === POOLS[0].id)
 	// NOTE: When there is old account with rewards
 	// and 20 000 on the identity - if the identity is not deployed
 	// If you gasless re-stake the rewards you will stake the amount on
@@ -186,24 +158,6 @@ const Gasless = () => {
 		onTxRes(res, "new-gasless-stake-snack")
 	}
 
-	const onReStake = async () => {
-		setReStakeOpen(false)
-		const res = await wrapDoingTxns(
-			restake.bind(null, chosenWalletType, stats, true)
-		)()
-
-		onTxRes(res, "new-gasless-re-stake-snack")
-	}
-
-	useEffect(() => {
-		setBond({
-			poolId: POOLS[0].id,
-			amount: userIdentityBalance
-		})
-	}, [identityAddr, userIdentityBalance, loaded])
-	
-	*/
-
 	return (
 		<Box>
 			<SectionHeader
@@ -227,7 +181,7 @@ const Gasless = () => {
 					{t("gasless.tempDisabled")}
 				</Typography>
 			</Box>
-			{/* <Box>
+			<Box>
 				<Box>
 					<Box>
 						<Box
@@ -262,24 +216,28 @@ const Gasless = () => {
 									boxShadow={25}
 								>
 									<Box
-										onClick={() => !identityAddr && setConnectWallet(true)}
+										onClick={() => !gaslessAddress && setConnectWallet(true)}
 										m={1}
 										mx={2}
 										classes={{ root: classes.address }}
-										{...(!identityAddr ? { style: { cursor: "pointer" } } : {})}
+										{...(!gaslessAddress
+											? { style: { cursor: "pointer" } }
+											: {})}
 									>
-										{identityAddr || t("gasless.connectWalletToSeeAddr")}
+										{gaslessAddress || t("gasless.connectWalletToSeeAddr")}
 									</Box>
-									{identityAddr && (
+									{gaslessAddress && (
 										<Box m={1}>
 											<IconButton
 												id="mobile-burger-btn"
 												color="secondary"
 												aria-label="open drawer"
 												onClick={() => {
-													copy(identityAddr)
+													copy(gaslessAddress)
 													addSnack(
-														t("gasless.copiedToClipboard", { identityAddr }),
+														t("gasless.copiedToClipboard", {
+															identityAddr: gaslessAddress
+														}),
 														"success"
 													)
 												}}
@@ -327,10 +285,10 @@ const Gasless = () => {
 											<Box>
 												{StatsCard({
 													size: "large",
-													loaded,
+													loaded: userDataLoaded,
 													title: t("gasless.adxBalanceOnAddr"),
-													subtitle: userIdentityBalance
-														? formatADXPretty(userIdentityBalance) + " ADX"
+													subtitle: gaslessAddrBalance
+														? formatADXPretty(gaslessAddrBalance) + " ADX"
 														: "",
 													extra:
 														canExecuteGaslessError || t("gasless.readyForStake")
@@ -364,56 +322,9 @@ const Gasless = () => {
 										</Tooltip>
 									</Box>
 								</Box>
-								{showReStake && (
-									<Box className={classes.actions}>
-										<Box mt={4}>
-											<Box>
-												{StatsCard({
-													size: "large",
-													loaded,
-													title: t("gasless.rewardsReadyForReStake"),
-													subtitle: tomRewardADX
-														? formatADXPretty(tomRewardADX) + " ADX"
-														: "",
-													extra:
-														canExecuteGaslessReStakeError ||
-														t("gasless.readyForReStake")
-												})}
-											</Box>
-										</Box>
-
-										<Box>
-											<Box mt={2}>
-												<Tooltip
-													title={
-														walletConnected
-															? canExecuteGaslessReStakeError || ""
-															: t("common.connectWallet")
-													}
-												>
-													<Box display="inline-block">
-														<Button
-															id={`re-stake-gasless-form-open`}
-															// fullWidth
-															variant="contained"
-															disableElevation
-															color="secondary"
-															size="large"
-															onClick={() => setReStakeOpen(true)}
-															disabled={!!canExecuteGaslessReStakeError}
-														>
-															{t("gasless.reStakeRewards")}
-														</Button>
-													</Box>
-												</Tooltip>
-											</Box>
-										</Box>
-									</Box>
-								)}
 							</Box>
 							<Box mt={2}>
-								{(!canExecuteGaslessError ||
-									!canExecuteGaslessReStakeError) && (
+								{!canExecuteGaslessError && (
 									<Box mt={2}>
 										<Trans
 											i18nKey="gasless.info"
@@ -423,7 +334,7 @@ const Gasless = () => {
 														color="inherit"
 														id="new-bond-form-adex-network-tos"
 														target="_blank"
-														href={`https://etherscan.io/address/${identityAddr}`}
+														href={`https://etherscan.io/address/${gaslessAddress}`}
 													/>
 												)
 											}}
@@ -445,40 +356,9 @@ const Gasless = () => {
 						timeout: 300
 					}}
 				>
-					<Fade in={bondOpen}>
-						{NewGaslessBondForm({
-							bond,
-							onStake,
-							chosenWalletType
-						})}
-					</Fade>
+					<Fade in={bondOpen}>{NewGaslessBondForm()}</Fade>
 				</Modal>
-
-				{ConfirmationDialog({
-					isOpen: reStakeOpen,
-					onDeny: () => setReStakeOpen(false),
-					onConfirm: () => {
-						onReStake()
-					},
-					confirmActionName: t("common.reStake"),
-					content: (
-						<Trans
-							i18nKey="dialogs.reStakeConfirmation"
-							values={{
-								amount: formatADXPretty(tomRewardADX ? tomRewardADX : ZERO),
-								currency: "ADX",
-								unbondDays: UNBOND_DAYS,
-								extraInfo: !stats.userBonds.find(x => x.status === "Active")
-									? t("dialogs.reActivatingInfo")
-									: ""
-							}}
-							components={{
-								box: <Box mb={2}></Box>
-							}}
-						/>
-					)
-				})}
-			</Box> */}
+			</Box>
 		</Box>
 	)
 }
