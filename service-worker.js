efd89ad8aa240c1cc7abb2db34a304d49503ce0c
Ivo Georgiev <ivo@strem.io>
2021-09-15 13:43:34 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.226ccb71518fc119dabcb209091888a3.js"
+  "/precache-manifest.61566ad366e841376c2505a639ce6779.js"
 );
 
 self.addEventListener('message', (event) => {
