6fae18137f0c0455c95cccd3d7e7b1765648ef48
IvoPaunov <ivo.paunov@gmail.com>
2022-02-01 22:29:12 +0200

    add web3reacrt root for walletconnect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import { HashRouter as Router } from "react-router-dom"
-import { Web3ReactProvider } from "@web3-react/core"
+import { Web3ReactProvider, createWeb3ReactRoot } from "@web3-react/core"
 import { ethers } from "ethers"
 import Root from "./components/Root"
 import AppContext from "./AppContext"
@@ -12,6 +12,7 @@ import Loading from "./components/Loading"
 import MultiThemeProvider from "./MultiThemeProvider"
 import { FarmProvider } from "./FarmProvider"
 import "./App.css"
+import { WALLET_CONNECT } from "./helpers/constants"
 
 const App = () => {
 	const appHooks = useApp()
@@ -32,14 +33,20 @@ const App = () => {
 	)
 }
 
+function getLibrary(provider) {
+	return new ethers.providers.Web3Provider(provider)
+}
+
+const Web3ReactProviderWalletConnect = createWeb3ReactRoot(WALLET_CONNECT)
+
 export default () => (
 	<MultiThemeProvider>
 		<CssBaseline />
 		<Suspense fallback={<Loading />}>
-			<Web3ReactProvider
-				getLibrary={provider => new ethers.providers.Web3Provider(provider)}
-			>
-				<App />
+			<Web3ReactProvider getLibrary={getLibrary}>
+				<Web3ReactProviderWalletConnect getLibrary={getLibrary}>
+					<App />
+				</Web3ReactProviderWalletConnect>
 			</Web3ReactProvider>
 		</Suspense>
 	</MultiThemeProvider>
